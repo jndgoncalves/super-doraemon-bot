e77baa8683ec946c9b3a774899633d584065cb28
jndgoncalves <joao.n.d.goncalves@gmail.com>
2022-05-18 16:12:21 +0100

    first commit, generated bot structure with Slappey, moved token from slappey config to dotenv

--- src/events/ready/ReadyEvent.ts
+++ src/events/ready/ReadyEvent.ts
@@ -5,7 +5,7 @@ export default class ReadyEvent extends BaseEvent {
   constructor() {
     super('ready');
   }
-  async run (client: DiscordClient) {
-    console.log('Bot has logged in.');
+  async run(client: DiscordClient) {
+    console.log('Super Doraemon has logged in.');
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-
+require('dotenv').config();
 import { registerCommands, registerEvents } from './utils/registry';
 import config from '../slappey.json';
 import DiscordClient from './client/client';
 import { Intents } from 'discord.js';
-const client = new DiscordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });
+const client = new DiscordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 (async () => {
   client.prefix = config.prefix || client.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
-  await client.login(config.token);
+  await client.login(process.env.BOT_TOKEN);
 })();
+
+
 
